d1d640943e9484fba3fba1c72f0a1b0973b9fcc2
ghandurathallah10 <ghandurathallah10@gmail.com>
2023-12-08 10:56:46 +0700

    refactor: typing typescript in edit modal

--- src/components/layout/EditModalForm.tsx
+++ src/components/layout/EditModalForm.tsx
@@ -1,29 +1,37 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
 import axios from 'axios';
+import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/router';
+import * as React from 'react';
 
-export function EditFormModal({ product }: any) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setLoading] = useState(false);
+import { Product } from '@/schema/product';
+
+type cardListProps = {
+  products: Product;
+};
+
+export function EditFormModal({ products }: cardListProps) {
   const router = useRouter();
 
-  const [formData, setFormData] = useState({
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isLoading, setLoading] = React.useState(false);
+
+  const [formData, setFormData] = React.useState({
     title: '',
     description: '',
     price: 0,
     image: '',
   });
 
-  useEffect(() => {
+  React.useEffect(() => {
     setFormData({
-      title: product?.title,
-      description: product?.description,
-      price: product?.price,
-      image: product?.image,
+      title: products?.title,
+      description: products?.description as string,
+      price: products?.price,
+      image: products?.image as string,
     });
-  }, [product]);
+  }, [products]);
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -34,17 +42,13 @@ export function EditFormModal({ product }: any) {
   const handleEdit = async () => {
     try {
       setLoading(true);
-      const responese = await axios.patch(`/api/product/${product.id}`, {
+      await axios.patch(`/api/product/${products.id}`, {
         title: formData.title,
         price: formData.price,
         image: formData.image,
         description: formData.description,
       });
-      console.log(responese);
-      if (responese.status == 200) {
-        setLoading(true);
-        router.push('/home');
-      }
+      router.push('/home');
     } catch (error) {
       console.error('Error editing product:', error);
     } finally {
@@ -75,7 +79,10 @@ export function EditFormModal({ product }: any) {
               className='fixed inset-0 bg-black/50'
               onClick={closeModal}
             ></div>
-            <div className='fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow '>
+            <form
+              onSubmit={handleEdit}
+              className='fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow '
+            >
               <div className='flex justify-between rounded-t border-b p-4 dark:border-gray-600 md:p-5'>
                 <h3 className='dark:text-dark text-xl font-semibold'>
                   Edit Product
@@ -161,34 +168,14 @@ export function EditFormModal({ product }: any) {
                   Cancel
                 </button>
                 <button
-                  type='button'
-                  onClick={handleEdit}
+                  type='submit'
                   className='mb-2 me-2 flex items-center rounded-full bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
                 >
                   Edit Product
-                  <div role='status'>
-                    {isLoading && (
-                      <svg
-                        aria-hidden='true'
-                        className='ml-2 h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600'
-                        viewBox='0 0 100 101'
-                        fill='none'
-                        xmlns='http://www.w3.org/2000/svg'
-                      >
-                        <path
-                          d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-                          fill='currentColor'
-                        />
-                        <path
-                          d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-                          fill='currentFill'
-                        />
-                      </svg>
-                    )}
-                  </div>
+                  <div role='status'>{isLoading && <Loader2 />}</div>
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         )}
       </div>
