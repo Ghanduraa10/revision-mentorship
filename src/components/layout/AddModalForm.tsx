6d3d85a6928fe9b7a7c1932fc3eb2f494f125174
ghandurathallah10 <ghandurathallah10@gmail.com>
2023-12-08 10:56:12 +0700

    refactor: typing typescript in add modal

--- src/components/layout/AddModalForm.tsx
+++ src/components/layout/AddModalForm.tsx
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Loader2, X } from 'lucide-react';
+import * as React from 'react';
 
-const AddModalForm = ({ setProduct }: any) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-  const [productName, setProductName] = useState('');
-  const [productPrice, setProductPrice] = useState<number>(0);
-  const [productImage, setProductImage] = useState('');
-  const [productDescription, setProductDescription] = useState('');
+import { api } from '@/lib/api';
+
+import { Product } from '@/schema/product';
+
+api;
+type AddModalFormProps = {
+  setProduct: React.Dispatch<React.SetStateAction<Product[]>>;
+};
+
+const AddModalForm = ({ setProduct }: AddModalFormProps) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isLoading, setLoading] = React.useState(false);
+  const [productName, setProductName] = React.useState('');
+  const [productPrice, setProductPrice] = React.useState(0);
+  const [productImage, setProductImage] = React.useState('');
+  const [productDescription, setProductDescription] = React.useState('');
 
   const openModal = () => {
     setIsOpen(true);
@@ -17,7 +26,9 @@ const AddModalForm = ({ setProduct }: any) => {
     setIsOpen(false);
   };
 
-  const handleAddProduct = async () => {
+  const handleAddProduct = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     if (
       productName === '' ||
       productDescription === '' ||
@@ -30,25 +41,14 @@ const AddModalForm = ({ setProduct }: any) => {
 
     try {
       setLoading(true);
-      const response = await axios.post('/api/product', {
+      const response = await api.post<{ data: Product }>('/api/product', {
         title: productName,
         price: productPrice,
         image: productImage,
         description: productDescription,
       });
-      if (response.status === 201) {
-        console.log('Product added successfully');
-        const newProduct = {
-          id: response.data.id,
-          title: productName,
-          price: productPrice,
-          image: productImage,
-          description: productDescription,
-        };
-        setProduct((prevState: any) => [...prevState, newProduct]);
-      } else {
-        console.error('Failed to add product');
-      }
+      const createdProduct = response.data.data;
+      setProduct((prevState) => [...prevState, createdProduct]);
     } catch (error) {
       console.error('Error adding product:', error);
     } finally {
@@ -67,7 +67,10 @@ const AddModalForm = ({ setProduct }: any) => {
       {isOpen && (
         <div>
           <div className='fixed inset-0 bg-black/50' onClick={closeModal}></div>
-          <div className='fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow '>
+          <form
+            onSubmit={(_e) => handleAddProduct(_e)}
+            className='fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow '
+          >
             <div className='flex items-center justify-between rounded-t border-b p-4 dark:border-gray-600 md:p-5'>
               <h3 className='dark:text-dark text-xl font-semibold'>
                 Add Product
@@ -78,21 +81,7 @@ const AddModalForm = ({ setProduct }: any) => {
                 data-modal-hide='default-modal'
                 onClick={closeModal}
               >
-                <svg
-                  className='h-3 w-3'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 14 14'
-                >
-                  <path
-                    stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
-                  />
-                </svg>
+                <X />
                 <span className='sr-only'>Close modal</span>
               </button>
             </div>
@@ -149,34 +138,14 @@ const AddModalForm = ({ setProduct }: any) => {
                 Cancel
               </button>
               <button
-                type='button'
-                onClick={handleAddProduct}
+                type='submit'
                 className='mb-2 me-2 flex items-center rounded-full bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
               >
                 Add Product
-                <div role='status'>
-                  {isLoading && (
-                    <svg
-                      aria-hidden='true'
-                      className='ml-2 h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600'
-                      viewBox='0 0 100 101'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-                        fill='currentColor'
-                      />
-                      <path
-                        d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-                        fill='currentFill'
-                      />
-                    </svg>
-                  )}
-                </div>
+                <div role='status'>{isLoading && <Loader2 />}</div>
               </button>
             </div>
-          </div>
+          </form>
         </div>
       )}
     </div>
