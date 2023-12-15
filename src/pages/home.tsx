b0bd0e78a2006a937a9d9beb0d0d1e3459a5bab7
ghandurathallah10 <ghandurathallah10@gmail.com>
2023-12-15 14:14:14 +0700

    feat: fetching product with react-query

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,35 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
 import { Loader2 } from 'lucide-react';
-import { useEffect, useState } from 'react';
-
-import { api } from '@/lib/api';
 
 import CardList from '@/components/layout/CardList';
 import Navbar from '@/components/layout/Navbar';
 
-import { Product } from '@/schema/product';
+import { fetchProduct } from '@/pages/api/product/productApi';
 
 export default function Home() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [isLoading, setLoading] = useState(false);
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useQuery({ queryKey: ['todos'], queryFn: fetchProduct });
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  if (isLoading) {
+    return (
+      <span>
+        <Loader2 />
+      </span>
+    );
+  }
 
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const response = await api.get<{ data: Product[] }>('/api/product');
-      setProducts(response.data.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  if (error) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
     <>
-      <Navbar setProduct={setProducts} />;
+      <Navbar setProduct={products} />;
       <div className='mt-32'>
         <div role='status' className='flex justify-center'>
           {isLoading && <Loader2 />}
